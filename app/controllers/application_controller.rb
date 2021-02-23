43125bc9f3c6879278436d23b9c72873cc3acc50
Auriane Gregoire <auriane.gregoire@gmail.com>
2021-02-23 17:18:39 +0100

    add first name last name and level to user & add user status to booking

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,14 @@ class ApplicationController < ActionController::Base
   #   flash[:alert] = "You are not authorized to perform this action."
   #   redirect_to(root_path)
   # end
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :level])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :level])
+  end 
+
 
   private
 
---
+++ db/migrate/20210223161334_add_atributes_to_users.rb
@@ -0,0 +1,7 @@
+class AddAtributesToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :level, :string
+  end
+end
