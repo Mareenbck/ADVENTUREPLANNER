0d5e8c7bcf079b6012de222bbb8bd0ccd9f070d5
Marine Bascunana <marine.bascunana@gmail.com>
2021-02-26 14:35:04 +0100

    fixer

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,9 +3,9 @@ class DashboardsController < ApplicationController
   def show
     @bookings = current_user.bookings
     if params[:bookings] == "last_bookings"
-      @bookings = @bookings.where('DATE(end_date) < ?', Date.today)
+      @bookings = @bookings.where('DATE(begin_date) < ?', Date.today)
     else
-      @bookings = @bookings.where('DATE(end_date) >= ?', Date.today)
+      @bookings = @bookings.where('DATE(begin_date) >= ?', Date.today)
     end
 
     authorize current_user
