3c4ca1c77e49a8f03828d71aa70290e8d686f94f
Auriane Gregoire <auriane.gregoire@gmail.com>
2021-03-03 12:58:19 +0100

    indexchatvf

--- app/policies/chatroom_policy.rb
+++ app/policies/chatroom_policy.rb
@@ -2,11 +2,6 @@ class ChatroomPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
       user.chatrooms
-      # scope.joins(bookings: [:user]).where(bookings: {user: user})
     end
-  end
-
-  def show?
-    true
   end
 end
