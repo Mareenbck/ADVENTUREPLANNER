547cce2a81423f3405ee003c504910b7c955c88a
Marine Bascunana <marine.bascunana@gmail.com>
2021-03-04 11:46:43 +0100

    wesh on a un chat

--- app/javascript/controllers/chat_controller.js
+++ app/javascript/controllers/chat_controller.js
@@ -16,14 +16,40 @@ export default class extends Controller {
 
   send(event) {
     event.preventDefault();
+    // console.log(document.getElementById("new_message"))
+    const formData = new FormData(document.getElementById("new_message"));
 
     fetchWithToken(`/chatrooms/${this.chatroomIdValue}/messages`,
       {
-        headers: { accept: "application/json", "Content-Type": "application/json" },
-        method: "POST"
+        headers: { accept: "application/json" },
+        method: "POST",
+        body: formData
       })
       .then(response => response.json())
       .then((data) => {
+        console.log(data.user_photo)
+        const date = {weekday: 'short', month: 'short', day: 'numeric' };
+        const time = { hour: 'numeric', minute: 'numeric' };
+
+        const message = ` <div class="message current-user" id="<message-${data.message_id}">
+            <div class="author">
+              <span>${data.author}</span>
+              <img class="chat-user-photo" src="http://res.cloudinary.com/ddw7wju1q/image/upload/${data.user_photo}">
+            </div>
+            <div class="message-content">
+              <p>${data.message}</p>
+              <small>${new Date().toLocaleString('en', date)} at ${new Date().toLocaleString('en', time)}</small>
+            </div>
+          </div>`
+        const chat = document.getElementById("chat-content");
+        chat.insertAdjacentHTML('beforeend', message);
+        document.querySelector("#chat .message:last-child").scrollIntoView({behavior: 'smooth'})
       });
+
+    const input = document.getElementById("message_content");
+    input.value = "";
+
+
   }
+
 }
