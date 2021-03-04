2a7cd697cc104f2889eb1a00b7c2caa98ce8b5ea
Loic Pauthenet <loic.pauthenet@gmail.com>
2021-03-04 17:26:35 +0100

    css modifs

--- app/javascript/plugins/init_popup.js
+++ app/javascript/plugins/init_popup.js
@@ -1,15 +1,18 @@
 const initPopup = () => {
   const popupFrame = document.querySelector('.popupframe');
+  const footer = document.querySelector('.footer');
   if (!popupFrame) return ;
 
   const planTrip = document.querySelector('.popupbtn');
   planTrip.addEventListener('click', () => {
     popupFrame.classList.remove('d-none');
+    footer.style.zIndex = "4";
   })
-  
+
   const close = document.querySelector('.popupclose');
   close.addEventListener('click', () => {
     popupFrame.classList.add('d-none');
+    footer.style.zIndex = "6";
   })
 }
 
