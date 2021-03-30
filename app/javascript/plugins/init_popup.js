915af4360f0d3a7145b23bcd01cb8ab37ae9cae1
Auriane Gregoire <auriane.gregoire@gmail.com>
2021-03-30 11:00:49 +0200

    bug btn book solve

--- app/javascript/plugins/init_popup.js
+++ app/javascript/plugins/init_popup.js
@@ -1,18 +1,21 @@
 const initPopup = () => {
   const popupFrame = document.querySelector('.popupframe');
   const footer = document.querySelector('.footer');
-  if (!popupFrame) return ;
+  if (!popupFrame) return;
+
 
   const planTrip = document.querySelector('.popupbtn');
   planTrip.addEventListener('click', () => {
+    console.log('plus la classe d none');
     popupFrame.classList.remove('d-none');
-    // footer.style.zIndex = "4";
+    console.log('plus la classe d none')
+    footer.style.zIndex = "4";
   })
 
   const close = document.querySelector('.popupclose');
   close.addEventListener('click', () => {
     popupFrame.classList.add('d-none');
-    // footer.style.zIndex = "6";
+    footer.style.zIndex = "6";
   })
 }
 
